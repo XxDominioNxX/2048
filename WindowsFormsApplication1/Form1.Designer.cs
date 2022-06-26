cd769bbc67448cb8b0e9636134a469fd59c42f01
XxDominioNxX <92040109+XxDominioNxX@users.noreply.github.com>
2022-06-26 15:58:33 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSize = true;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel1.Location = new System.Drawing.Point(13, 236);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(533, 533);
+            this.panel1.TabIndex = 0;
+            this.panel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.Color.OrangeRed;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(-1, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 62);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(13, 118);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(239, 98);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SCORE";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(307, 118);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(239, 98);
+            this.panel3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "BEST";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.BackColor = System.Drawing.Color.OrangeRed;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(-1, 32);
+            this.label4.Margin = new System.Windows.Forms.Padding(4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 62);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Tomato;
+            this.label5.Location = new System.Drawing.Point(13, -1);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(533, 105);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "2048";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(568, 782);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Press);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Button[,] list = new Button[4, 4];
+        Random rand = new Random();
+        const int size = 4;
+        const int Font = 35;
+
+
+        public Form1()
+        {
+            Color colorInit = Color.Black;
+            
+            
+            Init(list);
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Add();
+            Add();
+
+        }
+
+        //----------------------------------------------------------------------------------------------------------------------------------
+
+
+        private void Add()
+        {
+
+            int a = rand.Next(4);
+            int b = rand.Next(4);
+            if (list[a, b].Text != "-1")
+            {
+                if (Check())
+                {
+                    Add();
+                }
+                else
+                {
+                    MessageBox.Show("Места больше нет");
+                    return;
+                }
+            }
+            else
+            {
+
+                list[a, b].Location = new Point(Link_x(a), Link_y(b));
+                list[a, b].Text = "2";//a.ToString() + b.ToString();
+                
+                list[a, b].Font = new Font("Arial", Font, FontStyle.Bold);
+                Controls.Add(list[a, b]);
+
+            }
+
+
+        }
+
+        private bool Check()
+        {
+            int length = 4;
+            bool flag = false;
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < length; j++)
+                {
+                    if (list[i, j].Text == "-1")
+                    {
+                        flag = true;
+                    }
+                }
+            return flag;
+        }
+
+        private void Init(Button[,] list)
+        {
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                    list[i, j] = new Button();
+                    list[i, j].Text = "-1";
+                    list[i, j].Width = 100;
+                    list[i, j].Height = 100;
+                    list[i, j].Enabled = false;
+                    list[i, j].BackColor = Color.DarkOrchid;
+                }
+        }
+
+        private int Link_x(int x)
+        {
+
+            switch (x)
+            {
+                case 0:
+                    {
+                        x = (panel1.Width / 4) * 0 + panel1.Location.X;
+                        break;
+                    }
+                case 1:
+                    {
+                        x = (panel1.Width / 4) * 1 + panel1.Location.X;
+                        break;
+                    }
+                case 2:
+                    {
+                        x = (panel1.Width / 4) * 2 + panel1.Location.X;
+                        break;
+                    }
+                case 3:
+                    {
+                        x = (panel1.Width / 4) * 3 + panel1.Location.X;
+                        break;
+                    }
+
+
+            }
+
+            return x;
+        }
+
+        private int Link_y(int y)
+        {
+
+            switch (y)
+            {
+                case 0:
+                    {
+                        y = (panel1.Height / 4) * 0 + panel1.Location.Y;
+                        break;
+                    }
+                case 1:
+                    {
+                        y = (panel1.Height / 4) * 1 + panel1.Location.Y;
+                        break;
+                    }
+                case 2:
+                    {
+                        y = (panel1.Height / 4) * 2 + panel1.Location.Y;
+                        break;
+                    }
+                case 3:
+                    {
+                        y = (panel1.Height / 4) * 3 + panel1.Location.Y;
+                        break;
+                    }
+            }
+
+            return y;
+        }
+
+        private void Press(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode.ToString() == Keys.Left.ToString())
+            {
+                label1.Text = "Left";
+                Slide_Left();
+                Add();
+                Add();
+            }
+            if (e.KeyCode.ToString() == Keys.Up.ToString())
+            {
+                label1.Text = "Up";
+                Slide_Up();
+                Add();
+                Add();
+
+
+            }
+            if (e.KeyCode.ToString() == Keys.Right.ToString())
+            {
+                label1.Text = "Right";
+                Slide_Right();
+                Add();
+                Add();
+
+            }
+            if (e.KeyCode.ToString() == Keys.Down.ToString())
+            {
+                label1.Text = "Down";
+                Slide_Down();
+                Add();
+                Add();
+            }
+        }
+        //----------------------------------------------------------------------------------------------------------------------------
+        private void Slide_Left()
+        {
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == 0 || list[i, j].Text == "-1")
+                    {
+                        continue;
+                    }
+
+                    if (list[i - 1, j].Text != "-1")//суммирование
+                    {
+
+                        if (list[i - 1, j].Text == list[i, j].Text)
+                        {
+                            
+                            list[i - 1, j].Text = Convert.ToString(Convert.ToInt32(list[i - 1, j].Text) + Convert.ToInt32(list[i, j].Text));
+                            list[i, j].Text = "-1";
+                            Controls.Remove(list[i, j]);
+                        }
+                    }
+            
+                       
+                    
+                    if (list[i - 1, j].Text == "-1")//приближение збс
+                    {
+                        int a = i;
+                        int b = j;
+                        while (a != 0)
+                        {
+                            if (list[a - 1, b].Text == "-1")
+                            {
+                                list[a - 1, b].Location = new Point(Link_x(a - 1), Link_y(b));
+                                list[a - 1, b].Text = list[a, b].Text;
+                                list[a - 1, b].Font = new Font("Arial", Font, FontStyle.Bold);
+                                list[a, b].Text = "-1";
+                                Controls.Add(list[a - 1, b]);
+                                Controls.Remove(list[a, b]);
+
+                            }else
+                            {
+                                if (list[a - 1, b].Text == list[a, b].Text)
+                                {
+
+                                    list[a - 1, b].Text = Convert.ToString(Convert.ToInt32(list[a - 1, b].Text) + Convert.ToInt32(list[a, b].Text));
+                                    list[a, b].Text = "-1";
+                                    Controls.Remove(list[a, b]);
+                                }
+                            }
+                            --a;
+                        }
+
+                        
+                    }
+                }
+
+                
+            }
+            
+        }
+
+        private void Slide_Right()
+        {
+
+            for (int i = 3; i != -1; i--)
+            {
+                for (int j = 3; j != -1; j--)
+                {
+                    if (i == 3 || list[i, j].Text == "-1")
+                    {
+                        continue;
+                    }
+
+                    if (list[i + 1, j].Text != "-1")//суммирование
+                    {
+
+                        if (list[i + 1, j].Text == list[i, j].Text)
+                        {
+
+                            list[i + 1, j].Text = Convert.ToString(Convert.ToInt32(list[i + 1, j].Text) + Convert.ToInt32(list[i, j].Text));
+                            list[i, j].Text = "-1";
+                            Controls.Remove(list[i, j]);
+                        }
+                    }
+
+
+
+                    if (list[i + 1, j].Text == "-1")//приближение збс
+                    {
+                        int a = i;
+                        int b = j;
+                        while (a != 3)
+                        {
+                            if (list[a + 1, b].Text == "-1")
+                            {
+                                list[a + 1, b].Location = new Point(Link_x(a + 1), Link_y(b));
+                                list[a + 1, b].Text = list[a, b].Text;
+                                list[a + 1, b].Font = new Font("Arial", Font, FontStyle.Bold);
+                                list[a, b].Text = "-1";
+                                Controls.Add(list[a + 1, b]);
+                                Controls.Remove(list[a, b]);
+
+                            }
+                            else
+                            {
+                                if (list[a + 1, b].Text == list[a, b].Text)
+                                {
+
+                                    list[a + 1, b].Text = Convert.ToString(Convert.ToInt32(list[a + 1, b].Text) + Convert.ToInt32(list[a, b].Text));
+                                    list[a, b].Text = "-1";
+                                    Controls.Remove(list[a, b]);
+                                }
+                            }
+                            ++a;
+                        }
+
+
+                    }
+                }
+
+
+            }
+
+        }
+
+        private void Slide_Up()
+        {
+            bool up = false;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == 0 || list[j, i].Text == "-1")
+                    {
+                        continue;
+                    }
+                    if (list[j, i - 1].Text != "-1")
+                    {
+
+                        if (list[j, i - 1].Text == list[j, i].Text)
+                        {
+                            up = true;
+                            list[j, i - 1].Text = Convert.ToString(Convert.ToInt32(list[j, i - 1].Text) + Convert.ToInt32(list[j, i].Text));
+                            list[j, i].Text = "-1";
+                            Controls.Remove(list[j, i]);
+                        }
+                    }
+                    if (list[j, i - 1].Text == "-1")
+                    {
+                        up = true;
+                        list[j, i - 1].Location = new Point(Link_x(j), Link_y(i - 1));
+                        list[j, i - 1].Text = list[j, i].Text;
+                        list[j, i - 1].Font = new Font("Arial", Font, FontStyle.Bold);
+                        list[j, i].Text = "-1";
+                        Controls.Remove(list[j, i]);
+                        Controls.Add(list[j, i - 1]);
+                    }
+
+                }
+            }
+            if (up)
+            {
+                Slide_Up();
+            }
+        }
+
+        private void Slide_Down()
+        {
+            bool down = false;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == 3 || list[j, i].Text == "-1")
+                    {
+                        continue;
+                    }
+                    if (list[j, i + 1].Text != "-1")
+                    {
+
+                        if (list[j, i + 1].Text == list[j, i].Text)
+                        {
+                            down = true;
+                            list[j, i + 1].Text = Convert.ToString(Convert.ToInt32(list[j, i + 1].Text) + Convert.ToInt32(list[j, i].Text));
+                            list[j, i].Text = "-1";
+                            Controls.Remove(list[j, i]);
+                        }
+                    }
+                    if (list[j, i + 1].Text == "-1")
+                    {
+                        down = true;
+                        list[j, i + 1].Location = new Point(Link_x(j), Link_y(i + 1));
+                        list[j, i + 1].Text = list[j, i].Text;
+                        list[j, i + 1].Font = new Font("Arial", Font, FontStyle.Bold);
+                        list[j, i].Text = "-1";
+                        Controls.Remove(list[j, i]);
+                        Controls.Add(list[j, i + 1]);
+                    }
+
+                }
+            }
+            if (down)
+            {
+                Slide_Down();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+    }
+}
